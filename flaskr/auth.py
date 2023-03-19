189f68412781f94e2cc0d1573b20676c5f614307
jason zou <jasonzou@buffalo.edu>
2023-03-18 20:34:44 -0400

    register form in register.html

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -47,32 +47,25 @@ def login():
 
 @bp.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == "POST":
+    form = RegisterForm()
+    if form.validate_on_submit():
         username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
         db = get_db()
-        error = None
 
-        if not username:
-            error = "Username is required."
-        elif not password:
-            error = "Password is required."
+        try:
+            db.execute(
+                "INSERT INTO user (username, email, password) VALUES (?, ?, ?)",
+                (username, email, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+        else:
+            return redirect(url_for("auth.login"))
 
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template("auth/register.html")
+    return render_template("auth/register.html", form=form)
 
 
 @bp.before_app_request
