8ac53eba6a60a4e64093dbd108a952a3752e225f
jason zou <jasonzou@buffalo.edu>
2023-03-19 04:56:01 -0400

    admin content

--- flaskr/home.py
+++ flaskr/home.py
@@ -13,6 +13,19 @@ bp = Blueprint("home", __name__)
 @bp.route("/", methods=("GET", "POST"))
 @bp.route("/home", methods=("GET", "POST"))
 def home():
+    db = get_db()
+    users = None
+    if session.get("is_admin"):
+        try:
+            users = db.execute("Select * from user").fetchall()
+            for row in users:
+                print(*row)
+        except db.IntegrityError:
+            pass
+
     return render_template(
-        "home.html", user=session.get("user"), is_admin=session.get("is_admin")
+        "home.html",
+        user=session.get("user"),
+        is_admin=session.get("is_admin"),
+        users=users,
     )
